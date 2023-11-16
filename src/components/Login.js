504428087065bc7a8821b433d82461795db4098d
Bharath-S-V <svbharath545@gmail.com>
2023-11-16 08:50:39 +0530

    Login page create

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='login_bg_gradient bg-cover h-screen grid place-items-center'>login</div>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,30 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
+import { getSession, useSession } from 'next-auth/react';
+import Login from '@/components/Login';
 
-const inter = Inter({ subsets: ['latin'] })
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+const {data: session}=useSession
+if(!session) return <Login/>;
   return (
-    <main>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </main>
-  )
+    <>
+      <Head>
+        <tite>Netlix</tite>
+      </Head>
+      <main>We Have Session</main>
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  return {
+    props: {
+      session,
+    },
+  };
 }
